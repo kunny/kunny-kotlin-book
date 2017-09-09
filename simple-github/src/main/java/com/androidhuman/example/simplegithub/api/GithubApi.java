3287489dab38a9ccf0fbd9cdf4453f712557e486
Taeho Kim <jyte82@gmail.com>
2017-09-10 00:12:28 +0900

    Modify indent

--- simple-github/src/main/java/com/androidhuman/example/simplegithub/api/GithubApi.java
+++ simple-github/src/main/java/com/androidhuman/example/simplegithub/api/GithubApi.java
@@ -11,8 +11,7 @@ import retrofit2.http.Query;
 public interface GithubApi {
 
     @GET("search/repositories")
-    Call<RepoSearchResponse> searchRepository(
-            @Query("q") String query);
+    Call<RepoSearchResponse> searchRepository(@Query("q") String query);
 
     @GET("repos/{owner}/{name}")
     Call<GithubRepo> getRepository(@Path("owner") String ownerLogin, @Path("name") String repoName);
