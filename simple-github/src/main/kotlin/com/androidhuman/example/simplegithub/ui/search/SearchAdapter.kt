736bba651309d53ffd2481e098115e82c33f6944
Taeho Kim <jyte82@gmail.com>
2017-09-13 20:47:03 +0900

    Apply Kotlin Android Extensions (ViewHolder)

--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchAdapter.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchAdapter.kt
@@ -6,11 +6,10 @@ import android.support.v7.widget.RecyclerView
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.androidhuman.example.simplegithub.R
 import com.androidhuman.example.simplegithub.api.model.GithubRepo
 import com.androidhuman.example.simplegithub.ui.GlideApp
+import kotlinx.android.synthetic.main.item_repository.view.*
 import java.util.ArrayList
 
 class SearchAdapter : RecyclerView.Adapter<SearchAdapter.RepositoryHolder>() {
@@ -28,20 +27,22 @@ class SearchAdapter : RecyclerView.Adapter<SearchAdapter.RepositoryHolder>() {
     override fun onBindViewHolder(holder: RepositoryHolder, position: Int) {
         val repo = items[position]
 
-        GlideApp.with(holder.itemView.context)
-                .load(repo.owner.avatarUrl)
-                .placeholder(placeholder)
-                .into(holder.ivProfile)
+        with(holder.itemView) {
+            GlideApp.with(context)
+                    .load(repo.owner.avatarUrl)
+                    .placeholder(placeholder)
+                    .into(ivItemRepositoryProfile)
 
-        holder.tvName.text = repo.fullName
-        holder.tvLanguage.text = if (TextUtils.isEmpty(repo.language))
-            holder.itemView.context.getText(R.string.no_language_specified)
-        else
-            repo.language
+            tvItemRepositoryName.text = repo.fullName
+            tvItemRepositoryLanguage.text = if (TextUtils.isEmpty(repo.language))
+                context.getText(R.string.no_language_specified)
+            else
+                repo.language
 
-        holder.itemView.setOnClickListener {
-            if (null != listener) {
-                listener!!.onItemClick(repo)
+            setOnClickListener {
+                if (null != listener) {
+                    listener!!.onItemClick(repo)
+                }
             }
         }
     }
@@ -64,21 +65,7 @@ class SearchAdapter : RecyclerView.Adapter<SearchAdapter.RepositoryHolder>() {
 
     class RepositoryHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
             LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_repository, parent, false)) {
-
-        var ivProfile: ImageView
-
-        var tvName: TextView
-
-        var tvLanguage: TextView
-
-        init {
-
-            ivProfile = itemView.findViewById(R.id.ivItemRepositoryProfile)
-            tvName = itemView.findViewById(R.id.tvItemRepositoryName)
-            tvLanguage = itemView.findViewById(R.id.tvItemRepositoryLanguage)
-        }
-    }
+                    .inflate(R.layout.item_repository, parent, false))
 
     interface ItemClickListener {
 
