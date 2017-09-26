cc668274fd7dab7abcbf7334027f46fe824fb21e
Taeho Kim <jyte82@gmail.com>
2017-09-27 02:44:07 +0900

    Fix in operator overloading

--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/extensions/LifecycleExtensions.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/extensions/LifecycleExtensions.kt
@@ -1,7 +1,7 @@
 package com.androidhuman.example.simplegithub.extensions
 
 import android.arch.lifecycle.Lifecycle
-import com.androidhuman.example.simplegithub.rx.LifecycleDisposable
+import android.arch.lifecycle.LifecycleObserver
 
-operator fun Lifecycle.plusAssign(observer: LifecycleDisposable)
+operator fun Lifecycle.plusAssign(observer: LifecycleObserver)
         = this.addObserver(observer)
--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/extensions/RxJavaExtensions.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/extensions/RxJavaExtensions.kt
@@ -3,6 +3,5 @@ package com.androidhuman.example.simplegithub.extensions
 import com.androidhuman.example.simplegithub.rx.LifecycleDisposable
 import io.reactivex.disposables.Disposable
 
-operator fun LifecycleDisposable.plusAssign(disposable: Disposable) {
-    this.add(disposable)
-}
+operator fun LifecycleDisposable.plusAssign(disposable: Disposable)
+        = this.add(disposable)
