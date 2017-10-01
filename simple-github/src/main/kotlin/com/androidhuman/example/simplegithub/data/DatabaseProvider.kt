66b8ac38e83969dfcea452b5d185f8c710ac0636
Taeho Kim <jyte82@gmail.com>
2017-10-01 15:42:29 +0900

    Reuse database instance

--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/data/DatabaseProvider.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/data/DatabaseProvider.kt
@@ -3,10 +3,16 @@ package com.androidhuman.example.simplegithub.data
 import android.arch.persistence.room.Room
 import android.content.Context
 
+private var instance: SimpleGithubDatabase? = null
+
 fun provideSearchHistoryDao(context: Context): SearchHistoryDao
         = provideDatabase(context).searchHistoryDao()
 
-private fun provideDatabase(context: Context): SimpleGithubDatabase
-        = Room.databaseBuilder(context.applicationContext,
-        SimpleGithubDatabase::class.java, "simple_github.db")
-        .build()
+private fun provideDatabase(context: Context): SimpleGithubDatabase {
+    if (null == instance) {
+        instance = Room.databaseBuilder(context.applicationContext,
+                SimpleGithubDatabase::class.java, "simple_github.db")
+                .build()
+    }
+    return instance!!
+}
