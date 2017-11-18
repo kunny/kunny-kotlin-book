0c5a684721e04220635b993424d3b5d8ed1f0079
Taeho Kim <jyte82@gmail.com>
2017-11-18 18:15:03 +0900

    Move constant field placement

--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchActivity.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchActivity.kt
@@ -14,8 +14,6 @@ import com.androidhuman.example.simplegithub.api.model.GithubRepo
 import com.androidhuman.example.simplegithub.api.provideGithubApi
 import com.androidhuman.example.simplegithub.extensions.plusAssign
 import com.androidhuman.example.simplegithub.rx.AutoClearedDisposable
-import com.androidhuman.example.simplegithub.ui.repo.KEY_REPO_NAME
-import com.androidhuman.example.simplegithub.ui.repo.KEY_USER_LOGIN
 import com.androidhuman.example.simplegithub.ui.repo.RepositoryActivity
 import com.jakewharton.rxbinding2.support.v7.widget.queryTextChangeEvents
 import io.reactivex.Observable
@@ -87,8 +85,8 @@ class SearchActivity : AppCompatActivity(), SearchAdapter.ItemClickListener {
 
     override fun onItemClick(repository: GithubRepo) {
         startActivity<RepositoryActivity>(
-                KEY_USER_LOGIN to repository.owner.login,
-                KEY_REPO_NAME to repository.name)
+                RepositoryActivity.KEY_USER_LOGIN to repository.owner.login,
+                RepositoryActivity.KEY_REPO_NAME to repository.name)
     }
 
     private fun searchRepository(query: String) {
